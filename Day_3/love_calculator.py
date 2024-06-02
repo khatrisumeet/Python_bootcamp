28387845df49e0b32557f79dc0a25bdf6953e7c0
khatrisumeet <97824972+khatrisumeet@users.noreply.github.com>
2024-06-02 08:41:40 +0530

    Add files via upload

---
+++ Day_3/love_calculator.py
@@ -0,0 +1,20 @@
+print("the love calculator is calculating you score.....")
+name1 = input()
+name2 = input()
+
+combined_names = name1 + name2
+lower_names = combined_names.lower()
+t = lower_names.count("t")
+r = lower_names.count("r")
+u = lower_names.count("u")
+e = lower_names.count("e")
+first_digit = t+r+u+e
+
+l = lower_names.count("l")
+o = lower_names.count("o")
+v = lower_names.count("v")
+e = lower_names.count("e")
+second_digit = l+o+v+e
+
+score = str(first_digit) + str(second_digit)
+print(score)
